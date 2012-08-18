9e73918031fcddbbcbd63275639dba3b599130e1
Kyle McKee <mckee.kyle@gmail.com>
2012-08-18 11:58:50 -0400

    Close to having something functional.  Need to figure out how to call cd function.

--- extension/chrome/content/fireframe/fireframe.js
+++ extension/chrome/content/fireframe/fireframe.js
@@ -1,11 +1,47 @@
 FBL.ns(function() { with (FBL) {
 
   Firebug.FireFrameModel = extend(Firebug.Module, {
+    
     onSelectFrame: function(context) {
-        var panel = context.getPanel('console');
-        panel.inspectable = true;
-        Firebug.Inspector.startInspecting(context);
-        panel.inspectable = false;
+      var panel = context.getPanel('console');
+      panel.inspectable = true;
+      Firebug.Inspector.startInspecting(context);
+      panel.inspectable = false;
+    },
+    
+    getParentFrame: function(element)
+    {
+      return element.ownerDocument.defaultView.frameElement;
+    },
+
+    getFrameStackRecursive: function(element, frames)
+    {
+      frames = frames || [];
+      var parent = this.getParentFrame(element);
+      if (parent)
+      {
+          frames.push(parent);
+          this.getFrameStackRecursive(parent, frames);
+      }
+      return frames;
+    },
+
+    walkFrameStack: function(frames)
+    {
+      Firebug.Console.log('1');
+      cd(top);
+      Firebug.Console.log('2');
+      var currentFrame = frames.pop();
+      while (currentFrame)
+      {
+          this.cd(currentFrame.contentWindow);
+          currentFrame = frames.pop();
+      }
+    },
+
+    attachConsoleToFrameContaining: function(element)
+    {
+      this.walkFrameStack(this.getFrameStackRecursive(element));
     }
   });
 
@@ -22,7 +58,7 @@ FBL.ns(function() { with (FBL) {
     if (canceled === true)
       return;
 
-    alert('Node selected.  Need to walk iframe stack and target it.');
+    Firebug.FireFrameModel.attachConsoleToFrameContaining(node);
   }
 
   Firebug.ConsolePanel.prototype.supportsObject = function(object, type) {
@@ -32,7 +68,5 @@ FBL.ns(function() { with (FBL) {
 
   Firebug.ConsolePanel.prototype.inspectHighlightColor = "green";
 
-
   Firebug.registerModule(Firebug.FireFrameModel);
-
 }});
