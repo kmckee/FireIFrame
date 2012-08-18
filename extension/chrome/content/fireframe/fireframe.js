d6c244336b421d34133f40e12997618fd00daec8
Kyle McKee <mckee.kyle@gmail.com>
2012-08-17 21:33:18 -0400

    Removed panel

--- extension/chrome/content/fireframe/fireframe.js
+++ extension/chrome/content/fireframe/fireframe.js
@@ -1,30 +1,12 @@
 FBL.ns(function() { with (FBL) {
 
-  var panelName = "Frames";
   Firebug.FireFrameModel = extend(Firebug.Module,
   {
-    showPanel: function(browser, panel) {
-        var isHwPanel = panel && panel.name == panelName;
-        var hwButtons = browser.chrome.$("fbFireFrameButtons");
-        collapse(hwButtons, !isHwPanel);
-    },
     onMyButton: function(context) {
         alert("hi");
     }
   });
 
-  function FireFramePanel() {}
-  FireFramePanel.prototype = extend(Firebug.Panel,
-  {
-    name: panelName,
-    title: "Frames",
-
-    initialize: function() {
-      Firebug.Panel.initialize.apply(this, arguments);
-    },
-  });
-
   Firebug.registerModule(Firebug.FireFrameModel);
-  Firebug.registerPanel(FireFramePanel);
 
 }});
