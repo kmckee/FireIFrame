bb0f8095e19a77772c121dde6bbcd3c2b1ee8360
Kyle McKee <mckee.kyle@gmail.com>
2012-08-18 00:37:39 -0400

    Add basic selector functionality.

--- extension/chrome/content/fireframe/fireframe.js
+++ extension/chrome/content/fireframe/fireframe.js
@@ -1,11 +1,37 @@
 FBL.ns(function() { with (FBL) {
 
-  Firebug.FireFrameModel = extend(Firebug.Module,
-  {
+  Firebug.FireFrameModel = extend(Firebug.Module, {
     onSelectFrame: function(context) {
-        alert("hi");
+        var panel = context.getPanel('console');
+        panel.inspectable = true;
+        Firebug.Inspector.startInspecting(context);
+        panel.inspectable = false;
     }
   });
+
+
+  Firebug.ConsolePanel.prototype.startInspecting = function() {
+  
+  }
+
+  Firebug.ConsolePanel.prototype.inspectNode = function(node) {
+    return false;
+  }
+
+  Firebug.ConsolePanel.prototype.stopInspecting = function(node, canceled) {
+    if (canceled === true)
+      return;
+
+    alert('Node selected.  Need to walk iframe stack and target it.');
+  }
+
+  Firebug.ConsolePanel.prototype.supportsObject = function(object, type) {
+    var tagName = object.tagName.toLowerCase();
+    return (tagName == "iframe") ? 1 : 0;
+  }
+
+  Firebug.ConsolePanel.prototype.inspectHighlightColor = "green";
+
 
   Firebug.registerModule(Firebug.FireFrameModel);
 
